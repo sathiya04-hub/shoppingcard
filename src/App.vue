30664379d92a7426bc0f3eb18f0a8b84bc3a1545
Sathiyamoorthi Loganathan <sathiya.loganathan@vml.com>
2026-01-29 13:05:43 +0800

    Push initial code

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import FoodItem from './components/FoodItem.vue'
+
+</script>
+
+<template>
+  <h1>Food</h1>
+  <div id="wrapper">
+    <food-item 
+      food-name="Apples" 
+      food-desc="Apples are a type of fruit that grow on trees."/>
+    <food-item 
+      food-name="Pizza"
+      food-desc="Pizza has a bread base with tomato sauce, cheese, and toppings on top."/>
+    <food-item 
+      food-name="Rice"
+      food-desc="Rice is a type of grain that people like to eat."/>
+  </div>
+</template>
+
+
+<style>
+  #wrapper {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  #wrapper > div {
+    border: dashed black 1px;
+    flex-basis: 120px;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
+</style>                  
---
+++ src/components/FoodItem.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>{{ foodName }}</h2>
+        <p>{{ foodDesc }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'foodName',
+        'foodDesc'
+    ]
+};
+</script>
+
+<style>
+  img {
+    height: 1.5em;
+    float: right;
+  }
+</style>
